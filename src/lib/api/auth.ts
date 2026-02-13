6674352f3c3a6ac6835301a36570daf59e112f5c
mby444 <bimayudha161@gmail.com>
2026-02-13 15:37:22 +0700

    Remove additional try catch block

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,18 +1,12 @@
 import { apiClient, setToken, removeToken } from "./client";
 import type { LoginDto, RegisterDto, AuthResponse } from "@/src/types/user";
 
-export async function register(
-  data: RegisterDto,
-): Promise<AuthResponse | undefined> {
-  try {
-    const response = await apiClient.post<AuthResponse>(
-      "/api/auth/register",
-      data,
-    );
-    return response.data;
-  } catch (error) {
-    console.log("error", error);
-  }
+export async function register(data: RegisterDto): Promise<AuthResponse> {
+  const response = await apiClient.post<AuthResponse>(
+    "/api/auth/register",
+    data,
+  );
+  return response.data;
 }
 
 export async function login(credentials: LoginDto): Promise<AuthResponse> {
